e2a2eea06982062918e871336f1f37cc8cc50f2a
Shreyansh <sj3082002@gmail.com>
2024-11-04 17:53:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HeroSection } from "./components/HeroSection";
+import { Navbar } from "./components/Navbar";
+import "./App.css"
+
+const App=()=>{
+  return <>
+  <Navbar/>
+  <HeroSection/>
+  </>
+}
+
+export default App;
